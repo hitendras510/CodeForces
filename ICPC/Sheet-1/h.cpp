a20b85496534094d4e9e3a4241acbf4de57f1a0b
Hitendra Singh <hitendrasingh5489@gmail.com>
2026-05-01 21:33:35 +0530

    init

--- ICPC/Sheet-1/h.cpp
+++ ICPC/Sheet-1/h.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 
 int main(){
-    int a,b;
-    cin >> a >>b;
+    double A,B;
+    cin >> A >> B;
 
+    double x = A/B;
 
+    cout <<"floor " <<A<<" / "<<B<<" = "<<floor(x)<<endl;
+    cout <<"ceil " <<A<<" / "<<B<<" = "<<ceil(x)<<endl;
+    cout <<"round " <<A<<" / "<<B<<" = "<<round(x)<<endl;
 }
