07f78d1b5426d913e38b57a3e7368bc2a790d904
Hitendra Singh <hitendrasingh5489@gmail.com>
2026-05-08 12:58:00 +0530

    init

---
+++ ICPC/Sheet-2/a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a;
+
+    scanf("%d", &a);
+
+        for(int i=1;i<=a;i++){
+            printf("%d\n",i);
+        }
+    return 0;
+}
--- ICPC/Sheet-2/b.c
+++ ICPC/Sheet-2/b.c
@@ -7,7 +7,13 @@ int main() {
 
     scanf("%d", &a);
 
-    
+    if(a<2){
+        printf("-1\n");
+        return 0;
+    }
 
+    for(int i=2;i <= a;i+=2){
+        printf("%d\n",i);
+    }
     return 0;
 }
