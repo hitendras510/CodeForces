7c1b311022124a10d904ac16312a23515c75a723
Hitendra Singh <hitendrasingh5489@gmail.com>
2026-05-18 23:47:21 +0530

    init

--- ICPC/Sheet-2/h.c
+++ ICPC/Sheet-2/h.c
@@ -6,7 +6,23 @@ int main() {
     int a;
     scanf("%d", &a);
 
-    
+    int prime =1 ;
+
+    if(a <= 1){
+        prime = 0;
+    }else {
+        for(int i = 2;i <= sqrt(a);i++){
+            if(a % i == 0){
+                prime =0;
+                break;
+            }
+        }
+    }
+    if(prime){
+        printf("YES\n");
+    }else{
+        printf("NO\n");
+    }
 
     return 0;
 }
